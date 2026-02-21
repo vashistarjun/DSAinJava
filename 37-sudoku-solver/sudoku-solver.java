45549a35765392998c614866deacec2117a9fbe3
arjun vashist <145341493+vashistarjun@users.noreply.github.com>
2026-02-21 23:55:03 +0530

    Time: 378 ms (26.27%) | Memory: 43.1 MB (31.64%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+       boolean s= solve(board);
+       
+    }
+    public boolean solve(char [][] board){
+        
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+               if(board[i][j]=='.'){
+                for(char k='1';k<='9';k++){
+                if(safe(i,j,k,board)){
+                    board[i][j]=k;
+                   if(solve(board)) return true;
+                   else board[i][j]='.';
+                }
+               }
+               return false;
+               }
+            }
+        }
+        return true;
+    }
+    public boolean safe(int row,int col,char ch,char [][] board){
+        for(int i=0;i<9;i++){
+            if(board[i][col]==ch) return false;
+            if(board[row][i]==ch) return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==ch) return false;
+        }
+        return true;
+    }
+}
