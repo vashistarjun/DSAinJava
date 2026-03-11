fdda9973a5a7dd8787a9c69f484de957ec0e4d45
arjun vashist <145341493+vashistarjun@users.noreply.github.com>
2026-03-11 17:32:32 +0530

    Time: 31 ms (12.84%) | Memory: 58.7 MB (5.00%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -4,17 +4,18 @@ class Solution {
         int n=text1.length();
         int m=text2.length();
         dp= new Integer[n][m];
-        return helper(0,0,text1,text2);
-        
+        return f( 0, 0, text1,text2);
     }
-    public int helper(int i,int j,String s1,String s2){
-        if(i==s1.length() || j==s2.length()) return 0;
-        if(dp[i][j]!=null) return dp[i][j];
-        if(s1.charAt(i)==s2.charAt(j)){
-            return dp[i][j]=1+helper(i+1,j+1,s1,s2);
+    public int f(int i,int j,String text1,String text2){
+        if(i==text1.length() || j==text2.length()){
+            return 0;
         }
-        else{
-            return dp[i][j]=Math.max(helper(i+1,j,s1,s2),helper(i,j+1,s1,s2));
+        if(dp[i][j]!=null) return dp[i][j];
+        if(text1.charAt(i)==text2.charAt(j)){
+            return 1+f(i+1,j+1,text1,text2);
+        }
+        else {
+            return dp[i][j]=Math.max(f(i+1,j,text1,text2),f(i,j+1,text1,text2));
         }
     }
 }
