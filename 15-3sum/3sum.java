1112898a4c4c78173d7a48dffe0e44d6de58fa45
arjun vashist <145341493+vashistarjun@users.noreply.github.com>
2026-03-27 15:04:39 +0530

    Time: 36 ms (30.51%) | Memory: 59.9 MB (18.14%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,34 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        Set<List<Integer>> l= new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-          
+        int n=nums.length;
+        List<List<Integer>> list= new ArrayList<>();
+        for(int i=0;i<n;i++){
+             if(i > 0 && nums[i] == nums[i - 1]) continue;
             int left=i+1;
-            int right=nums.length-1;
+            int right=n-1;
             while(left<right){
                 int sum=nums[i]+nums[left]+nums[right];
                 if(sum==0){
-                    List<Integer> list= new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[left]);
-                    list.add(nums[right]);
-                    Collections.sort(list);
-                    l.add(list);
-                    while(left<right && nums[left]==nums[left+1]) left++;
-                    while(left<right && nums[right]==nums[right-1]) right--;
-                    left++;
-                    right--;
+                    List<Integer> l= new ArrayList<>();
+                    l.add(nums[i]);
+                    l.add(nums[left]);
+                    l.add(nums[right]);
+                    list.add(l);
+                
+                while(left<right && nums[left]==nums[left+1]) left++;
+                while(left<right && nums[right]==nums[right-1]) right--;
+                left++;
+                right--;
                 }
-                else if(sum<0){
-                    left++;
-
-                }
-                else{
-                    right--;
-                }
+                else if(sum<0) left++;
+                else right--;
             }
         }
-        return new ArrayList<>(l);
+        return list;
     }
 }
