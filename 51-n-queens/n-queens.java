81cc668be53e3f4e9458a0f62a1776691d1667bc
arjun vashist <145341493+vashistarjun@users.noreply.github.com>
2026-04-02 13:46:23 +0530

    Time: 2 ms (82.20%) | Memory: 46.7 MB (36.02%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,53 +1,55 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
+        char mat[][]= new char[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(mat[i],'.');
+        }
         List<List<String>> list= new ArrayList<>();
-        int arr[][]= new int[n][n];
-        
-        solve(0,arr,list);
-       return list;
-
+        List<String> l= new ArrayList<>();
+        backtrack(0,list,n,mat);
+        return list;
     }
-    public void solve(int col,int arr[][],List<List<String>> list){
-
-        if(col==arr.length){
-             List<String> l= new ArrayList<>();
-             for(int i=0;i<arr.length;i++){
-                StringBuilder sb= new StringBuilder();
-                for(int j=0;j<arr.length;j++){
-                    if(arr[i][j]==0) sb.append('.');
-                    else sb.append('Q');
+    public void backtrack(int col,List<List<String>> list,int n,char
+    mat[][]){
+        if(col==n){
+            List<String> l= new ArrayList<>();
+            for(int i=0;i<n;i++){
+             StringBuilder sb= new StringBuilder();
+                for(int j=0;j<n;j++){
+                   sb.append(mat[i][j]);
                 }
                 l.add(sb.toString());
-             }
-             list.add(l);
+              
+            }
+            list.add(l);
+
             return ;
         }
-        
-        for(int i=0;i<arr.length;i++){
-           if (safe(i, col, arr)) {
-
-                arr[i][col] = 1;
-
-                solve(col + 1, arr, list);
-
-                arr[i][col] =0;
+        for(int i=0;i<n;i++){
+            if(isSafe(i,col,mat)){
+                mat[i][col]='Q';
+                backtrack(col+1,list,n,mat);
+                 mat[i][col]='.';
             }
         }
-       
-    } 
-    public boolean safe(int row,int col,int arr[][]){
-        for(int i=col;i>=0;i--){
-            if(arr[row][i]==1) return false;
-        }
-        for(int i=row,j=col;i>=0 && j>=0 ;j--,i--){
-            if(arr[i][j]==1) return false;
-        }
-       for(int i=row,j=col;i<arr.length && j>=0;j--,i++){
-            if(arr[i][j]==1) return false;
-        }
-      
+        
+    }
+    public boolean isSafe(int row,int col,char mat[][]){
+        // same row (left side)
+    for(int j = col - 1; j >= 0; j--){
+        if(mat[row][j] == 'Q') return false;
+    }
 
-      
-        return true;
+    // upper-left diagonal
+    for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--){
+        if(mat[i][j] == 'Q') return false;
+    }
+
+    // lower-left diagonal
+    for(int i = row + 1, j = col - 1; i < mat.length && j >= 0; i++, j--){
+        if(mat[i][j] == 'Q') return false;
+    }
+
+    return true;
     }
 }
