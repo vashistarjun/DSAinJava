3109f32e71740b36c05497699e93f807dcb20908
arjun vashist <145341493+vashistarjun@users.noreply.github.com>
2026-04-20 23:49:35 +0530

    Time: 3 ms (98.48%) | Memory: 47.1 MB (55.78%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,33 +1,27 @@
 class Solution {
     Integer dp[][];
-
     public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-        dp = new Integer[n][m];
-
-        int lcs = f(0, 0, word1, word2);
-      return lcs;
+        int n=word1.length();
+        int m=word2.length();
+        dp= new Integer[n][m];
+        return f(0,0,word1,word2);
     }
-
-    int f(int i, int j, String word1, String word2) {
-        if (i == word1.length())
-            return word2.length() - j; // insert remaining
-
-        if (j == word2.length())
-            return word1.length() - i;
-
-        if (dp[i][j] != null) return dp[i][j];
-
-        if (word1.charAt(i) == word2.charAt(j)) {
-            return dp[i][j] = f(i + 1, j + 1, word1, word2);
-        } else {
-             // different characters → try all 3 ops
-        int insert  = f(i, j + 1, word1, word2);
-        int delete  = f(i + 1, j, word1, word2);
-        int replace = f(i + 1, j + 1, word1, word2);
-
-        return dp[i][j]=1 + Math.min(insert, Math.min(delete, replace));
+    public int f(int i,int j,String word1,String word2){
+        if(i==word1.length()){
+            return word2.length()-j;
+        }
+        if(j==word2.length()){
+            return word1.length()-i;
+        }
+        if(dp[i][j]!=null) return dp[i][j];
+        if(word1.charAt(i)==word2.charAt(j)){
+            return dp[i][j]=f(i+1,j+1,word1,word2);
+        }
+        else{
+            int insert=f(i,j+1,word1,word2);
+            int replace=f(i+1,j+1,word1,word2);
+            int delete=f(i+1,j,word1,word2);
+            return dp[i][j]=1 + Math.min(delete,Math.min(replace,insert));
         }
     }
 }
