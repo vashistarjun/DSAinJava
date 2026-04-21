d89011a94886c97b8d3fc2929a16130c1835745b
arjun vashist <145341493+vashistarjun@users.noreply.github.com>
2026-04-21 17:02:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (70.55%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -1,21 +1,26 @@
 class Solution {
-
     public List<String> wordBreak(String s, List<String> wordDict) {
-        List<String> list= new ArrayList<>();
-        List<String> path= new ArrayList<>();
-        backtrack(s,wordDict,list,path);
-        return list;
+         List<String> ans= new ArrayList<>();
+         List<String> path= new ArrayList<>();
+         backtrack(s,wordDict,ans,path);
+         return ans;
     }
-    public void backtrack(String s,List<String> wordList,List<String> list,List<String> path){
-        if(s.length()==0){     //ans mil gya
-        String a=String.join(" ",path);
-        list.add(a);
+    public void backtrack(String s,List<String> wordDict,List<String> ans,
+    List<String> path){
+        if(s.length()==0){
+            StringBuilder sb= new StringBuilder();
+            for(int i=0;i<path.size();i++){
+                String str=path.get(i);
+                sb.append(str);
+               if(i!=path.size()-1) sb.append(" ");
+            }
+            ans.add(sb.toString());
             return;
         }
-        for(String word:wordList){
-            if(s.startsWith(word)){
-                path.add(word);
-                backtrack(s.substring(word.length()),wordList,list,path);
+        for(String str:wordDict){
+            if(s.startsWith(str)){
+                path.add(str);
+                backtrack(s.substring(str.length()),wordDict,ans,path);
                 path.remove(path.size()-1);
             }
         }
