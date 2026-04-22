894624d378eac1b3f579efe3b3300e049e627e54
arjun vashist <145341493+vashistarjun@users.noreply.github.com>
2026-04-22 16:43:53 +0530

    Time: 1 ms (96.37%) | Memory: 41.4 MB (100.00%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -1,26 +1,32 @@
 class Solution {
     public String reorganizeString(String s) {
-        Map<Character,Integer> map= new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+        int freq[]= new int[26];
+        char maxchar='a';
+        int max=0;
+        for(char ch:s.toCharArray()){
+            freq[ch-'a']++;
+            if(max<freq[ch-'a']){
+                max=freq[ch-'a'];
+                maxchar=ch;
+            }
         }
-        PriorityQueue<Character> pq= new PriorityQueue<>((a,b)->map.get(b)-map.get(a));
-        pq.addAll(map.keySet());
-        if(map.get(pq.peek())> (s.length()+1)/2) return "";
-        
-        char result[]=new char[s.length()];
-        int i=0;
-        while(!pq.isEmpty()){
-            char ch=pq.poll();
-           for(int j=0;j<map.get(ch);j++){
-            if(i>=s.length()) i=1;
-            result[i]=ch;
+        if(max > (s.length() + 1) / 2) return "";
+       int i=0;
+        char arr[]= new char[s.length()];
+        while(freq[maxchar-'a']>0){
+            arr[i]=maxchar;
             i+=2;
-           }
+            freq[maxchar-'a']--;
         }
-         return new String(result);
-
-
+       
+        for(int j=0;j<26;j++){
+            while(freq[j]>0){
+                if(i>=s.length()) i=1;
+                arr[i]=(char) (j+'a');
+                i+=2;
+                freq[j]--;
+            }
+        }
+        return new String(arr);
     }
 }
