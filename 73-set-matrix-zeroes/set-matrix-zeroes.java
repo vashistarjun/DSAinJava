d137ee06a1e3e851ad0527d01ff3898bcfc2d143
arjun vashist <145341493+vashistarjun@users.noreply.github.com>
2026-05-12 17:06:00 +0530

    Time: 12 ms (11.61%) | Memory: 48.7 MB (5.21%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,43 +1,26 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-
-        int n = matrix.length;
-        int m = matrix[0].length;
-
-        boolean[][] visited = new boolean[n][m];
-        Queue<int[]> q = new LinkedList<>();
-
-        // Step 1: Add all zero cells to queue
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 0) {
-                    q.add(new int[]{i, j});
-                    visited[i][j] = true;
+        int n=matrix.length;
+        int m=matrix[0].length;
+            Queue<int[]> queue= new LinkedList<>();
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    if(matrix[i][j]==0){
+                        queue.add(new int[]{i,j});
+                    }
                 }
             }
-        }
-
-        // Step 2: BFS
-        while (!q.isEmpty()) {
-            int[] cell = q.poll();
-            int r = cell[0];
-            int c = cell[1];
-
-            // Zero full row
-            for (int col = 0; col < m; col++) {
-                if (!visited[r][col]) {
-                    matrix[r][col] = 0;
-                    visited[r][col] = true;
+            while(!queue.isEmpty()){
+                int arr[]= queue.poll();
+                int row=arr[0];
+                int col=arr[1];
+                for(int i=0;i<n;i++){
+                    matrix[i][col]=0;
+                }
+                for(int i=0;i<m;i++){
+                    matrix[row][i]=0;
                 }
             }
 
-            // Zero full column
-            for (int row = 0; row < n; row++) {
-                if (!visited[row][c]) {
-                    matrix[row][c] = 0;
-                    visited[row][c] = true;
-                }
-            }
-        }
     }
 }
